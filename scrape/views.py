05387c665c2d1a9af9056be922c79dde92b8b14e
Niroxapex1 <taherinima28@gmail.com>
2024-10-28 18:18:27 +0330

    Time Out Error Fixed

--- scrape/views.py
+++ scrape/views.py
@@ -1,22 +1,23 @@
 from decimal import Decimal
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
 import json
 from .models import ScrapeData
 from price.models import ExchangeRate
-from playwright.sync_api import sync_playwright
+from .serializers import ScrapeDataSerializer
 from selenium.common.exceptions import (NoSuchElementException as NSEE, TimeoutException as TOE, 
-                                         NoSuchWindowException as NSWE, InvalidArgumentException as IAE, WebDriverException)
+                                         NoSuchWindowException as NSWE, InvalidArgumentException as IAE, WebDriverException,StaleElementReferenceException as SERE,)
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-class ScrapeView(APIView):
+import time
 
+class ScrapeView(APIView):
     def post(self, request, *args, **kwargs):
         url = request.data.get('url')
         color = request.data.get('color')
@@ -30,7 +31,7 @@ class ScrapeView(APIView):
 
         try:
             # ارسال درخواست به API خارجی
-            response = requests.post('http://188.165.62.242:5049/scrape', json={
+            response = requests.post('http://185.80.196.109:5049/scrape', json={
                 "url": url,
                 "color": color,
                 "size": size,
@@ -55,25 +56,25 @@ class ScrapeView(APIView):
                 'response_data': json.dumps(response_data)  # ذخیره داده‌ها به صورت JSON
             }
         )
-
         try:
-            # دریافت نرخ ارز و دیگر مقادیر
+            # دریافت نرخ ارز
             exchange_rate = ExchangeRate.objects.first()
             if exchange_rate is None:
                 return Response({'error': 'Exchange rate not set'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # فقط مقادیر ثبت‌شده در مدل ExchangeRate را باز می‌گردانیم
-            converted_price = exchange_rate.aed_to_toman
-            additional_cost = exchange_rate.profit_percentage
+            # محاسبه قیمت تبدیل‌شده و هزینه‌ها
+            price = Decimal(response_data.get('results', {}).get('price', '0.0'))
+            converted_price = price * exchange_rate.aed_to_toman
+            additional_cost = converted_price * Decimal('0.25')
             shipping_cost = exchange_rate.shipping_cost
-            per_kg_cost = exchange_rate.per_kg_cost
+            total_cost = converted_price + additional_cost + shipping_cost
         except Exception as e:
             return Response({'error': f'Error processing data: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # تبدیل داده‌ها به فرمت خروجی
+        # تبدیل داده‌ها به فرمت مشابه با خروجی Namshi
         result = {
             'id': str(scrape_data.id),
-            'price': response_data.get('results', {}).get('price', 'Unknown'),  # قیمت اولیه
+            'price': str(price),
             'images': response_data.get('results', {}).get('images', []),
             'title': response_data.get('results', {}).get('title', ''),
             'color': response_data.get('results', {}).get('color', []),
@@ -81,14 +82,11 @@ class ScrapeView(APIView):
             'sizes': response_data.get('results', {}).get('sizes', []),
             'Flavor': response_data.get('results', {}).get('Flavor', []),
             'available': response_data.get('results', {}).get('available'),
-            'weight': response_data.get('results', {}).get('weight', 'Unknown'),  # اضافه کردن وزن
-            # مقادیر ذخیره‌شده در مدل
-            'aed_to_toman': str(converted_price),
-            'profit_percentage': str(additional_cost),
+            'converted_price': str(converted_price),
+            'additional_cost': str(additional_cost),
             'shipping_cost': str(shipping_cost),
-            'per_kg_cost': str(per_kg_cost),
+            'total_cost': str(total_cost)
         }
-
         return Response({'results': result}, status=status.HTTP_200_OK)
 
 class ScrapeNamshiView(APIView):
@@ -104,121 +102,110 @@ class ScrapeNamshiView(APIView):
                 return Response({'error': 'Failed to retrieve data from Namshi.'}, status=status.HTTP_400_BAD_REQUEST)
 
             try:
-                # دریافت نرخ ارز
+                # Fetch the exchange rate
                 exchange_rate = ExchangeRate.objects.first()
                 if exchange_rate is None:
                     return Response({'error': 'Exchange rate not set'}, status=status.HTTP_400_BAD_REQUEST)
-
-                # محاسبه قیمت تبدیل‌شده و هزینه‌ها
+                
+                # Calculate the converted price and additional costs
                 price = Decimal(data.get('price', '0.0').replace('د.إ.', '').strip())
-                # converted_price = price * exchange_rate.aed_to_toman
-                # additional_cost = converted_price * Decimal('0.25')
-                # shipping_cost = exchange_rate.shipping_cost
-                # total_cost = converted_price + additional_cost + shipping_cost
-                converted_price = exchange_rate.aed_to_toman
-                additional_cost = exchange_rate.profit_percentage
+                converted_price = price * exchange_rate.aed_to_toman
+                additional_cost = converted_price * Decimal('0.25')
                 shipping_cost = exchange_rate.shipping_cost
-                per_kg_cost = exchange_rate.per_kg_cost
+                total_cost = converted_price + additional_cost + shipping_cost
             except Exception as e:
                 return Response({'error': f'Error processing data: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
 
+            # Save scraped data
             scrape_data, created = ScrapeData.objects.update_or_create(
                 url=url,
                 defaults={
                     'color': data.get('color_source', []),
                     'size': data.get('size', []),
-                    'style': [],  # این بخش برای Namshi استفاده نمی‌شود
-                    'flavor': [],  # این بخش برای Namshi استفاده نمی‌شود
-                    'response_data': json.dumps(data)  # ذخیره داده‌ها به صورت JSON
+                    'style': [],  # Not used for Namshi
+                    'flavor': [],  # Not used for Namshi
+                    'response_data': json.dumps(data)  # Save data as JSON
                 }
             )
 
+            # Prepare the result
             result = {
                 'id': scrape_data.id,
                 'images': data.get('picture', []),
                 'title': data.get('name', ''),
                 'color': data.get('color_source', []),
-                'styles': [],  # این بخش برای Namshi استفاده نمی‌شود
+                'styles': [],  # Not used for Namshi
                 'sizes': data.get('size', []),
-                'flavor': [],  # این بخش برای Namshi استفاده نمی‌شود
-                'available': None,  # این اطلاعات از Namshi دریافت نمی‌شود
+                'flavor': [],  # Not used for Namshi
+                'available': None,  # Not provided by Namshi
                 'weight': 'Not Found',
-                'price' : data.get('price',''),
-                'aed_to_toman': str(converted_price),
-                'profit_percentage': str(additional_cost),
+                'converted_price': str(converted_price),
+                'additional_cost': str(additional_cost),
                 'shipping_cost': str(shipping_cost),
-                'per_kg_cost': str(per_kg_cost),
+                'total_cost': str(total_cost)
             }
 
             return Response({'results': result}, status=status.HTTP_200_OK)
+        except (TOE, SERE, WebDriverException) as e:
+            return Response({'error': f'Web scraping error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'error': f'Unexpected error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def namshi(url):
     driver = None
 
-    def driver_init():
+    def driver_init(retry_count=3):
         nonlocal driver
         options = Options()
         options.add_argument("--headless")
-        options.add_argument('--enable-gpu')  # Enable GPU for better rendering
-        options.add_argument('--no-sandbox')  # Required for certain environments (optional)
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("start-maximized")
-        # options.add_argument("disable-infobars")
-        # options.add_argument("--disable-extensions")
-        # options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--window-size=1366x768')
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-popup-blocking")
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-        try:
-            driver = webdriver.Chrome(options=options)
-            driver.set_page_load_timeout(60)
-            driver.get(url)
-            print('Finding End ...')
-            end = driver.find_element(By.XPATH, '/html/body/section/section/section/div[2]/div[1]/div/div/div[1]')
-            actions = ActionChains(driver)
-            actions.scroll_to_element(end).perform()
-        except (TOE, WebDriverException) as e:
-            print(f"Error initializing the driver: {e}")
-            if driver:
-                driver.quit()
-            raise
+        
+        for attempt in range(retry_count):
+            try:
+                driver = webdriver.Chrome(options=options)
+                driver.set_page_load_timeout(90)  # Increase timeout
+                driver.get(url)
+                end = driver.find_element(By.XPATH, '/html/body/section/section/section/div[2]/div[1]/div/div/div[1]')
+                actions = ActionChains(driver)
+                actions.scroll_to_element(end).perform()
+                break
+            except (TOE, WebDriverException) as e:
+                if driver:
+                    driver.quit()
+                if attempt < retry_count - 1:
+                    time.sleep(5)  # Wait before retrying
+                    continue
+                raise Exception(f"Error initializing the driver: {str(e)}")
         return driver
 
     def products():
         try:
-            print('Locating Brand ...')
             brand = driver.find_element(By.CLASS_NAME, 'ProductConversion_brand__Y76P_').text.strip()
-            print('Locating Name ...')
             name = driver.find_element(By.XPATH, '/html/body/section/section/section/section/div/div[2]/section[1]/section[1]/div/div[1]/h1').text.strip()
-            print('Locating Price ...')
             price = driver.find_element(By.XPATH, '/html/body/section/section/section/section/div/div[2]/section[1]/section[1]/section/div[1]/section/span/span[2]').text.replace('\n', ' ')
 
-            print('Finding Size_Div ...')
             size_div = driver.find_elements(By.XPATH, '/html/body/section/section/section/section/div/div[2]/section[1]/div[1]/div[2]/div')
-            print('Locating Sizes ...')
             size = [_.text.strip() for size1 in size_div for _ in size1.find_elements(By.CLASS_NAME, 'SizePills_size_variant__4qpXf')]
 
             color = []
             color_page = []
             try:
-                print('Locating Color_Big_Div ...')
                 color_big_div = driver.find_elements(By.CLASS_NAME, 'ProductConversion_groupImages__4tRb8')
-                print('Locating Colors ...')
                 for m in color_big_div:
                     color_mid_div = m.find_elements(By.TAG_NAME, 'img')
                     color.extend([color1.get_property("src") for color1 in color_mid_div])
-                print('Locating Page_colors ...')
                 pag = [page.get_property('href') for pg in color_big_div for page in pg.find_elements(By.TAG_NAME, 'a')]
                 color_page.extend(pag)
             except NSEE:
                 pass
 
             picture = []
-            print('Locating Pictures ...')
             picture_div = driver.find_elements(By.CLASS_NAME, 'ImageGallery_container__cNIDV')
             for pic1 in picture_div:
                 pic2 = pic1.find_elements(By.TAG_NAME, 'img')
@@ -234,9 +221,8 @@ def namshi(url):
                 'picture': picture,
             }
 
-        except (NSEE, NSWE, TOE, Exception) as e:
-            print(f"An unexpected error occurred: {e}")
-            raise
+        except (NSEE, TOE, SERE, Exception) as e:
+            raise Exception(f"An error occurred while scraping product data: {str(e)}")
 
     driver_init()
     try:
@@ -244,7 +230,7 @@ def namshi(url):
     finally:
         if driver:
             driver.quit()
-
+            
 class ScrapeDataDetailView(APIView):
     def get(self, request, *args, **kwargs):
         try:
@@ -281,10 +267,10 @@ class ScrapeDataDetailView(APIView):
             'sizes': response_data.get('sizes'),
             'flavor': response_data.get('flavor'),
             'available': response_data.get('available'),
-            'aed_to_toman': str(converted_price),
-            'profit_percentage': str(additional_cost),
+            'converted_price': str(converted_price),
+            'additional_cost': str(additional_cost),
             'shipping_cost': str(shipping_cost),
-            'per_kg_cost': str(total_cost)
+            'total_cost': str(total_cost)
         }
 
         return Response({'results': result}, status=status.HTTP_200_OK)
